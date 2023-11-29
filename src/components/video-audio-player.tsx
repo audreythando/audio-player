4a4fd5d4b68c5c447d53611dd7f4b62905209f42
AudreyMpalume <129782349+AudreyMpalume@users.noreply.github.com>
2023-11-29 11:37:43 +0200

    feat/fetch data using react-query

--- src/components/video-audio-player.tsx
+++ src/components/video-audio-player.tsx
@@ -1,52 +1,46 @@
 import React from 'react';
 import { Box, Card, CardContent, CardMedia, Typography } from '@mui/material';
 import VideoAudioService from "../services/video-audio-service";
-import { useQuery, useMutation } from "react-query";
+import { useQuery } from "react-query";
 import ReactPlayer from 'react-player';
 import toast from 'react-hot-toast';
 
 interface Audio {
-    Id: number;
-    Episode: string;
-    Title: string;
-    Media: number;
-    Description: string;
-    Image: string;
-    Icon: string;
-    Audio: string;
-    RemoteId: string;
-    Status: number;
-    isActive: number;
-    inId: string;
-    CreateDate: string;
-    MediaName: string;
-    Category: string;
-    Epoch: number;
-    AudioSize: number;
-    ImageUrl: string; 
-  }
-  
+  Id: number;
+  Episode: string;
+  Title: string;
+  Media: number;
+  Description: string;
+  Image: string;
+  Icon: string;
+  Audio: string;
+  RemoteId: string;
+  Status: number;
+  isActive: number;
+  inId: string;
+  CreateDate: string;
+  MediaName: string;
+  Category: string;
+  Epoch: number;
+  AudioSize: number;
+  ImageUrl: string; 
+}
 
-  const  AudioPlayer = () => {
+const AudioPlayer = () => {
+  const { data: audioData, error } = useQuery("get-users", VideoAudioService.getTimeline);
 
-  async function getUsers() {
-    try {
-      const response = await VideoAudioService.getTimeline();
-      return response.data;
-    } catch (error) {
-      toast.error("");
-      return [];
-    }
+  if (error) {
+    toast.error("Failed to fetch audio data");
   }
 
   return (
     <Box>
-      {audioData?.map((audio) => (
+      {audioData && audioData.map((audio: Audio) => (
         <Card key={audio.Id}>
           <CardMedia component="img" height="140" image={audio.Image} alt={audio.Title} />
           <CardContent>
             <Typography variant="h6">{audio.Title}</Typography>
-            <ReactPlayer url={`https://arthurfrost.qflo.co.za/${audio.Audio}`} controls />
+            <ReactPlayer url={audio.Audio} controls />
           </CardContent>
         </Card>
       ))}
@@ -55,4 +49,3 @@ interface Audio {
 }
 
 export default AudioPlayer;
-
